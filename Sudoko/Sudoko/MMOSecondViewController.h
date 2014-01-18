579c461e3bb179132727ca1fb0b9e185b3de5588
name <826878909@qq.com>
2014-01-18 23:10:33 +0800

    add[]:add a new page.

---
+++ Sudoko/Sudoko/MMOSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMOSecondViewController.h
+//  Sudoko
+//
+//  Created by 梅默 on 14-1-18.
+//  Copyright (c) 2014年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMOSecondViewController : UIViewController
+
+@end
---
+++ Sudoko/Sudoko/MMOSecondViewController.m
@@ -0,0 +1,41 @@
+//
+//  MMOSecondViewController.m
+//  Sudoko
+//
+//  Created by 梅默 on 14-1-18.
+//  Copyright (c) 2014年 BiXiaopeng. All rights reserved.
+//
+
+#import "MMOSecondViewController.h"
+
+@interface MMOSecondViewController ()
+
+@end
+
+@implementation MMOSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButtonPushed:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
